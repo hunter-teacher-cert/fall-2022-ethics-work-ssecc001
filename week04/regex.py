07a5958d241de6c0c7f3a9941e68fabc8cc09787
ssecc001 <>
2022-09-28 22:48:46 +0000

    regex

---
+++ week04/regex.py
@@ -0,0 +1,19 @@
+import re
+
+
+def find_date(line):
+    pattern = r"([A-Za-z][-,A-Za-z. ']+[ ]*)+"
+    result = re.findall(pattern,line)
+
+    pattern=r'((?:Mrs|Mr|Ms|Dr))'
+    result = result + re.findall(pattern,line)
+    return result
+
+
+f = open("names.txt")
+for line in f.readlines():
+    #print(line)
+    result = find_date(line)
+    if (len(result)>0):
+        print(result)
+
